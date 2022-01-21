b7feec3e1a51bcdc2c3c4a1d4e2acfde2282462d
aledc7 <alejandrocdecastro@gmail.com>
2022-01-21 19:55:41 -0300

    Primer contacto con los estados, cambiando un parrafo a partir de un input text

--- src/components/TodoSearch.js
+++ src/components/TodoSearch.js
@@ -2,18 +2,44 @@ import React from "react";
 import '../css/TodoSearch.css'
 
 function TodoSearch(){
+
+    // creo una constante para setear el estado del componente 
+
+    // useState hay que instanciarlo en un array de 2 elementos,
+    // los elementos son 2, el 'value' y una 'funcion' que cambia este value.
+
+    const [searchValue, setSearchValue] = React.useState('');
     
-    // defino una constante y le gardo una arrow funcion
-    // esta arrow funcion recive unas propiedades y luego las mete en el console.log
-    const onSearchValueChange = (event)=>console.log(event.target.value);
+
+    // creo una constanten onSearchValueChange y le asigno una arrow funcion que reciba un evento
+    // luego llamo a la funcion setSearchValue que es la funcion del estado que declaré arriba
+    // a esta funcion le paso como parámetro lo que el usuario esté escribiendo 'event.target.value'
+
+    // por último  en el input de busqueda esa funcion 'onSearchValueChange' la llamo usando el evento 'onChange'
+    // tambien hay que asignarle el 'value' del estado creado arriba
+
+
+    // finalmente puedo imprimir el value de ese estado en un h1.
+
+    const onSearchValueChange = (event)=>{
+        console.log(event.target.value);
+        setSearchValue(event.target.value)
+    };
 
     return (
         <>
             {/* este es un elemento con una clase, luego le pongo un icono html5 &#128270; */}
             <p className="Icon-search">&#128270;</p>
 
-            {/* est es otro elemento con un evento onchange, que recibe la constante, que dentro tiene una arrow funcion */}
-            <input className="TodoSearch" placeholder='Buscar Tarea' onChange={onSearchValueChange}/>
+
+            <input 
+                className="TodoSearch" 
+                placeholder='Buscar Tarea' 
+                onChange={onSearchValueChange}
+                value={searchValue}
+            />
+
+            <h1>{searchValue}</h1>
         </>
     );
 }
